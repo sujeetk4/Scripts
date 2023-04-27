7a440dec3bc38a38112f1038d4924028959b7145
sujeetk4 <109840250+sujeetk4@users.noreply.github.com>
2023-04-27 22:54:12 +0530

    Update k8sss.sh
    updated

--- k8sss.sh
+++ k8sss.sh
@@ -3,39 +3,38 @@
 -------------------------------------- Both Master & Worker Node ---------------------------------------
 
 sudo apt update -y
-sudo apt install docker.io -y
+sudo apt install docker.io -y      ---docker is requried for all
+sudo systemctl start docker        
+sudo systemctl enable docker    
 
-sudo systemctl start docker
-sudo systemctl enable docker
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg    (gets the kyes for kuber)
 
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list      (deb signe)
 
 sudo apt update -y
-sudo apt install kubeadm=1.20.0-00 kubectl=1.20.0-00 kubelet=1.20.0-00 -y
+sudo apt install kubeadm=1.20.0-00 kubectl=1.20.0-00 kubelet=1.20.0-00 -y   (intsall's tools)
 
 --------------------------------------------- Master Node -------------------------------------------------- 
-sudo su
-kubeadm init
+sudo su   (root user)
+kubeadm init   ( create all the tools in master)
 
   mkdir -p $HOME/.kube
   sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
   sudo chown $(id -u):$(id -g) $HOME/.kube/config
   
-kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml   (asks kubectl to create the network)
 
-kubeadm token create --print-join-command
+kubeadm token create --print-join-command     (creates the tooken to join the cluster)
   
   
 ------------------------------------------- Worker Node ------------------------------------------------ 
 sudo su
-kubeadm reset pre-flight checks
------> Paste the Join command on worker node with `--v=5`
+kubeadm reset pre-flight checks   (resets so that to join fresh server and makes master to worker)
+-----> Paste the Join command on worker node with `--v=5`   (creates kublet in worker from token)
 
 ---------------------------------------on Master Node-----------------------------------------
 
-kubectl get nodes 
+kubectl get nodes  
 
 
 # worker
